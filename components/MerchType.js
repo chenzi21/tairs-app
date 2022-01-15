62ce0a06cc88cbed3f4f411fa2904b2f59cc3b93
Chen Zadik <chenzg12345@gmail.com>
2022-01-15 23:52:25 +0800

    made progress with carousel

--- components/MerchType.js
+++ components/MerchType.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/MerchType.module.scss"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronCircleLeft, faChevronCircleRight } from "@fortawesome/free-solid-svg-icons";
 import SingleMerch from "./singleMerch";
+import RandomId from "../essentials/randomId"
+
+const merchWidth = 150
 
 const Merch = [
     {
@@ -11,36 +14,92 @@ const Merch = [
         price: "100$"
     },
     {
-        title: "air-jordans1",
+        title: "air-jordans",
         image: "someImage",
         price: "200$"
     },
     {
-        title: "air-jordans2",
+        title: "air-jordans",
         image: "someImage",
         price: "300$"
     },
     {
-        title: "air-jordans3",
+        title: "air-jordans",
+        image: "someImage",
+        price: "400$"
+    },
+    {
+        title: "air-jordans",
+        image: "someImage",
+        price: "400$"
+    },
+    {
+        title: "air-jordans",
+        image: "someImage",
+        price: "400$"
+    },
+    {
+        title: "air-jordans",
+        image: "someImage",
+        price: "400$"
+    },
+    {
+        title: "air-jordans",
+        image: "someImage",
+        price: "400$"
+    },
+    {
+        title: "air-jordans",
+        image: "someImage",
+        price: "400$"
+    },
+    {
+        title: "air-jordans",
+        image: "someImage",
+        price: "400$"
+    },
+    {
+        title: "air-jordans",
         image: "someImage",
         price: "400$"
     },
 ]
 
+
+
 export default function MerchType({ title }) {
+    const [transX, setTransX] = useState(0)
+
+    const clickRight = () => {
+        setTransX(() => {
+            return transX - 2
+        })
+        console.log(`right ${transX}`)
+    }
+
+    const clickLeft = () => {
+        setTransX(() => {
+            return transX + 2
+        })
+        console.log(`left ${transX}`)
+    }
+
     return (
         <div className={styles.MerchTypeContainer}>
             <div className={styles.merchContainerTitle}>
                 {title}
             </div>
             <div className={styles.merchContent}>
-                <FontAwesomeIcon icon={faChevronCircleLeft} />
-                {Merch.map(merch => {
+                <FontAwesomeIcon className={styles.icon} icon={faChevronCircleLeft} onClick={() => clickLeft()} />
+                {Merch.map((merch, i) => {
+                    console.log(i === (transX/2))
                     return (
-                        <SingleMerch title={merch.title} key={merch.title} price={merch.price} image={merch.image} />
+                        <div className={styles.sliderSlide} key={RandomId()} data-scale={i === (transX/2)} style={{ transform: `translateX(${(i + transX) * merchWidth + 75}px)`, minWidth: `${merchWidth}px`, transition: "transform 10s" }}>
+                            <SingleMerch title={merch.title + i} price={merch.price} image={merch.image} />
+                        </div>
                     )
                 })}
-                <FontAwesomeIcon icon={faChevronCircleRight} />
+                <FontAwesomeIcon className={styles.icon} icon={faChevronCircleRight} onClick={() => clickRight()} />
             </div>
         </div>
     )
