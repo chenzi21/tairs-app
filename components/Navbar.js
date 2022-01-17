8da245c917cadbd571b07c1209f96601e4edd14a
Chen Zadik <211814975@idf.il>
2022-01-17 16:03:02 +0200

    made navbar responsive to phone

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import NavCategory from "./NavCategory";
 import styles from "../styles/Navbar.module.scss";
 import Logo from "../images/logo.jpeg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEllipsisV } from "@fortawesome/free-solid-svg-icons";
 
 const categories = [
     "Merchandise",
@@ -11,14 +13,21 @@ const categories = [
 ];
 
 export default function Navbar() {
+    const [menuOpen, setMenuOpen] = useState(false);
+    const handleMenuOpen = () => {
+        setMenuOpen(!menuOpen);
+    };
     return (
         <div className={styles.navbar}>
             <img src={Logo.src} alt="" className={styles.logo} />
-            {categories.map(category => {
-                return (
-                    <NavCategory title={category} key={category} />
-                );
-            })}
+            <div className={styles.categories} data-menu-open={menuOpen}>
+                {categories.map(category => {
+                    return (
+                        <NavCategory title={category} key={category} />
+                    );
+                })}
+            </div>
+            <FontAwesomeIcon icon={faEllipsisV} className={styles.icon} onClick={handleMenuOpen} />
         </div>
     );
 }
